65e85ed463b0754f46fdfa6e9661ad437fec2641
Rohit Rai <rbahingrai143@gmail.com>
2024-11-22 01:18:59 +0530

    changes in cabins page functionality

---
+++ thewildoasis/src/app/cabins/data.jsx
@@ -0,0 +1,7 @@
+const cabinsData = [
+  { id: "001", capacity: "2", price: "350", discount: "25" },
+  { id: "001", capacity: "2", price: "450", discount: null },
+  { id: "001", capacity: "4", price: "100", discount: "30" },
+  { id: "002", capacity: "3", price: "200", discount: "15" },
+];
+export default cabinsData;
